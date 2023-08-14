f4a438a996dc99d8bd22566b8565de5f3cf85acb
gokutheengineer <engineerzubizaretta@gmail.com>
2023-08-14 16:19:57 +0300

    add stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,52 @@
+// stack node
+class Node<T> {
+    public value: T;
+    public next: Node<T> | undefined;
+}
+
 export default class Stack<T> {
     public length: number;
-
+    private top: Node<T> | undefined;
     
 
     constructor() {
+        this.length = 0;
+        this.top = undefined;
     }
 
     push(item: T): void {
+        const node = {value: item} as Node<T>;
+
+        this.length++;  
+
+        // if the stack is empty
+        if (!this.top) {
+            this.top = node;
+            return;
+        }
+
+        node.next = this.top;
+        this.top = node;
+
 
 }
     pop(): T | undefined {
+        if (!this.top) {
+            return undefined;
+        }
+
+        this.length--;
+        
+        const value = this.top.value;
+        this.top = this.top.next;
+
+        return value;  
 
 }
     peek(): T | undefined {
-
+        if (this.top === undefined) {
+            return undefined;
+        }
+        return this.top.value;
 }
 }
