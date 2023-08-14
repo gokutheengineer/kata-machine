e62535f5e59084f80737a5f66eb76d0021b77a43
gokutheengineer <engineerzubizaretta@gmail.com>
2023-08-14 16:49:14 +0300

    add two crystal balls solution

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,20 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    // sqrt
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
 
+    let end_index = jumpAmount;
+
+    for (; end_index < breaks.length; end_index+= jumpAmount) {
+        if (breaks[end_index]) {
+            break;
+        }
+    }
+    
+    for (let startIndex = end_index - jumpAmount; startIndex <= end_index; startIndex++) {
+        if (breaks[startIndex]) {
+            return startIndex;
+        }
+    }
+
+    return -1;
 }
