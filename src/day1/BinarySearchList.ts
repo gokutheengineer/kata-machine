83554dd9f886a1180b8879958f13b36d1bc9ea5e
gokutheengineer <engineerzubizaretta@gmail.com>
2023-08-25 18:42:12 +0300

    add binary search on list solution

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+    let lo = 0; 
+    let hi = haystack.length;
+    
+    do {
+        const med = Math.floor(lo + (hi - lo) / 2);
+        const value = haystack[med];
 
+        if (value === needle) {
+            return true;
+        } else if (value < needle) {
+            lo = med + 1
+        } else {
+            hi = med -1
+        }
+    } while (lo < hi);
+    
+    return false;
 }
