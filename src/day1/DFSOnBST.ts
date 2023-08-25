eb0795ef24205a276daad99e43579f9c8c3d8cfc
gokutheengineer <engineerzubizaretta@gmail.com>
2023-08-25 18:37:37 +0300

    add dfs on bst

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,19 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function dfs_search(curr: BinaryNode<number> | null, needle: number): boolean {
+    if(!curr) {
+        return false;
+    }
 
+    if (curr.value === needle) {
+        return true;
+    } else if (curr.value < needle) {
+        curr = curr.right
+    } else if (curr.value > needle) {
+        curr = curr.left
+    }
+
+    return dfs_search(curr, needle);
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return dfs_search(head, needle);
 }
